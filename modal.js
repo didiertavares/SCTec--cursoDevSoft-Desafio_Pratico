ee103e1cacd8f8fadafcfeb19aec4203ae47bf9e
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-03-23 18:07:49 -0300

    versº 5
    CSS concluído
    ajustes JS

---
+++ modal.js
@@ -0,0 +1,5 @@
+const projBox = document.getElementsByClassName('projBox')
+
+projBox.addEventListener('clic', {
+    console.log("projeto clicado")
+}
--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const menuList = document.getElementById("menuList")
 buttonMenu.addEventListener("click", function(){
     menuList.classList.toggle("ativa")
 })
+
+
+const projBox = document.querySelectorAll('.projBox')
+const projDiv = document.querySelector('#sobreProjetos div')
+
+
+projBox.forEach(projDiv => {
+    projDiv.addEventListener('click', function(){
+        console.log('projBox clicado', projDiv.id)
+    })
+})
